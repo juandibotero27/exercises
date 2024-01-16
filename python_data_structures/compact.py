1b90063e1f2cde879df6b7c22ba45fd2718f3e78
Juan Botero <152219137+juandibotero27@users.noreply.github.com>
2024-01-16 11:48:28 -0500

    Add files via upload

---
+++ python_data_structures/compact.py
@@ -0,0 +1,15 @@
+def compact(lst):
+    """Return a copy of lst with non-true elements removed.
+
+        >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
+        [1, 2, 'All done']
+    """
+    answer = []
+    for item in lst:
+        if item:
+            answer.append(item)
+    return answer
+
+         
+
+
